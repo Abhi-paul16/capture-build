655835056b68fae39c0513c7abe071b3c9b8d1f4
Abhishek Pal <abhishek1100861@gmail.com>
2021-11-22 23:05:28 +0530

    Movie section

---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styled from "styled-components";
+
+import {useHistory} from 'react-router-dom'
+
+const MovieDetail = ()=>{
+
+return(
+    <div>
+        <h1>Movie Details</h1>
+    </div>
+)
+}
+
+export default MovieDetail
